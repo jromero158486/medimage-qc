1f89e06e0745e2cc24c5c937763d14adcdd6290d
jromero158486 <romerojoselyn80@gmail.com>
2026-08-07 17:43:14 -0500

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+
+  basePath:
+    process.env.NODE_ENV === "production"
+      ? "/medimage-qc"
+      : "",
+
   reactStrictMode: true,
   poweredByHeader: false,
 };
